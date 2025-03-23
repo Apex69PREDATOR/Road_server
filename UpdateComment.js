1426158907139b065ba8d87b50ab8fffd6c9c4bb
Apex <arpana036@gmail.com>
2025-03-24 02:25:40 +0530

    cmt response

--- UpdateComment.js
+++ UpdateComment.js
@@ -1,8 +1,7 @@
 const Router=require("express").Router()
 const BlogModel=require("./BlogPostsSchema")
-const userModel=require("./Account/UserSchema")
 
-Router.post('/giveComment',async(req,res)=>{
+Router.post('/giveComment',async (req,res)=>{
     try{
     const {uid,bid,uname,comment}=req.body
     const Comment={uid:uid,name:uname,comment:comment}
@@ -18,7 +17,7 @@ Router.post('/giveComment',async(req,res)=>{
     return res.status(404).json({message:"try again letter"})
 }
 catch(err){
-    return res.status(500).json({err:err,message:"server is facing some issue try again letter"})
+    return res.status(500).json({err:err,message:"server is facing some issue try again later"})
 }    
 })
 
