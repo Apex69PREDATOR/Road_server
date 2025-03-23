80205e4b9fde7e46b9164dc2891663d7a0c08bdf
Apex <arpana036@gmail.com>
2025-03-24 01:13:12 +0530

    comment add logic

---
+++ UpdateComment.js
@@ -0,0 +1,25 @@
+const Router=require("express").Router()
+const BlogModel=require("./BlogPostsSchema")
+const userModel=require("./Account/UserSchema")
+
+Router.post('/giveComment',async(req,res)=>{
+    try{
+    const {uid,bid,uname,comment}=req.body
+    const Comment={uid:uid,name:uname,comment:comment}
+    console.log(uid);
+    const upddatedDoc=await BlogModel.findByIdAndUpdate(bid,{
+        $push:{CommentedBy:Comment},
+    },{new:true})
+
+    if(upddatedDoc){
+      return res.status(200).json({success:true,message:"commented successfully"})
+    }
+    
+    return res.status(404).json({message:"try again letter"})
+}
+catch(err){
+    return res.status(500).json({message:"server is facing some issue try again letter"})
+}    
+})
+
+module.exports=Router
